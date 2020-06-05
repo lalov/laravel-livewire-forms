db7b332f94109a42c92057c7a551776d864d4dff
Lars Erik Løvhaug <lalov@profixio.com>
2020-06-05 12:32:18 +0200

    --wip-- [skip ci]

--- resources/views/fields/error-help.blade.php
+++ resources/views/fields/error-help.blade.php
@@ -1,6 +1,6 @@
 @error($field->key)
     <div class="invalid-feedback d-block" role="alert">
-        <strong>{{ $this->errorMessage($message) }}</strong>
+        <strong>{{ $message }}</strong>
     </div>
 @elseif($field->help)
     <small class="form-text text-muted">{{ $field->help }}</small>
