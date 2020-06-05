1d1b8d4620379252a5055321fb956b59b9e97e3c
Lars Erik Løvhaug <lalov@profixio.com>
2020-06-05 14:42:05 +0200

    --wip-- [skip ci]

--- src/Field.php
+++ src/Field.php
@@ -20,6 +20,11 @@ class Field extends BaseField
 
     public function __construct($label, $name)
     {
+        if(is_null($name) && strtolower($label) == $label){
+            // Translate label like sfFormWidgets do
+            $name = $label;
+            $label = __(str_replace('_', ' ', ucfirst('_id' == substr($label, -3) ? substr($label, 0, -3) : $label)));
+        }
         $this->label = $label;
         $this->name = $name ?? Str::snake(Str::lower($label));
         $this->key = 'form_data.' . $this->name;
